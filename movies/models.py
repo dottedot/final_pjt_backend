049e56487450d131fbb1bdfc3cf34a33a6a9946b
tjdwlslrj <tjdwlslrj@naver.com>
2022-05-19 01:34:58 +0900

    first commit

---
+++ movies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Genres(models.Model):
+    genre = models.CharField(max_length=10)
+
+
+class Movies(models.Model):
+    genre = models.ForeignKey('Genres', on_delete=models.CASCADE)
+    tmdb_id = models.IntegerField()
+    title = models.CharField(max_length=50)
+    overview = models.TextField()
+    posterurl = models.TextField()
+    tagline = models.TextField()
+    release_date = models.DateTimeField(auto_now=False)
+    vote_average = models.FloatField() 
+    runtime = models.IntegerField()
